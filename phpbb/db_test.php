2d1bd724e0dcd8ca13803d91899e5e21054d0ea6
Neil Alderson <neil.alderson@gmail.com>
2025-11-06 13:36:35 +0000

    Use MYSQLI_INIT_COMMAND to set charset before connection

--- phpbb/db_test.php
+++ phpbb/db_test.php
@@ -51,13 +51,12 @@ echo "<h3>Test 3: MySQL Connection</h3>";
 $mysqli = mysqli_init();
 if ($mysqli) {
     @mysqli_options($mysqli, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-    @mysqli_real_connect($mysqli, $dbhost, $dbuser, $dbpasswd, $dbname, $dbport, null, MYSQLI_CLIENT_FOUND_ROWS);
-    if ($mysqli->connect_error) {
+    @mysqli_options($mysqli, MYSQLI_INIT_COMMAND, "SET NAMES utf8");
+    $connect_result = @mysqli_real_connect($mysqli, $dbhost, $dbuser, $dbpasswd, $dbname, $dbport, null, MYSQLI_CLIENT_FOUND_ROWS);
+    if (!$connect_result || $mysqli->connect_error) {
         echo "<span style='color:red'>⚠️ Connection failed: " . $mysqli->connect_error . "</span><br>";
         echo "Error code: " . $mysqli->connect_errno . "<br>";
     } else {
-        // Set charset after connection
-        @mysqli_set_charset($mysqli, 'utf8');
         echo "<span style='color:green'>✓ MySQL connection successful!</span><br>";
     
         // Test query
